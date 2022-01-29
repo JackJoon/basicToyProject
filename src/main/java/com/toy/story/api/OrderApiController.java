ec673625f05976dea14a7265806979d6d5f95b37
ljt8692dhdlxor@naver.com <dnjfrmq!29>
2022-01-30 00:38:46 +0900

    [feature] 강의 자료 샘플링 [desc] API 최적화 정리.

--- src/main/java/com/toy/story/api/OrderApiController.java
+++ src/main/java/com/toy/story/api/OrderApiController.java
@@ -108,11 +108,13 @@ public class OrderApiController {
         return result;
     }
 
+    //쿼리 1 + N번
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    //쿼리 두번에
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
@@ -122,6 +124,7 @@ public class OrderApiController {
     public List<OrderQueryDto> ordersV6() {
         List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
 
+        // Application으로 이용해서 처리한다.
         return flats.stream()
                 .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
                         mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
--- src/main/java/com/toy/story/repository/order/query/OrderQueryRepository.java
+++ src/main/java/com/toy/story/repository/order/query/OrderQueryRepository.java
@@ -36,7 +36,7 @@ public class OrderQueryRepository {
      */
     private List<OrderQueryDto> findOrders() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                "select new com.toy.story.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                         " from Order o" +
                         " join o.member m" +
                         " join o.delivery d", OrderQueryDto.class)
@@ -48,7 +48,7 @@ public class OrderQueryRepository {
      */
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                "select new com.toy.story.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
                         " where oi.order.id = : orderId", OrderItemQueryDto.class)
@@ -84,7 +84,7 @@ public class OrderQueryRepository {
 
     private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
         List<OrderItemQueryDto> orderItems = em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                "select new com.toy.story.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
                         " where oi.order.id in :orderIds", OrderItemQueryDto.class)
@@ -92,12 +92,12 @@ public class OrderQueryRepository {
                 .getResultList();
 
         return orderItems.stream()
-                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId)); //Map으로 변환
     }
 
     public List<OrderFlatDto> findAllByDto_flat() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                "select new com.toy.story.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
                         " from Order o" +
                         " join o.member m" +
                         " join o.delivery d" +
